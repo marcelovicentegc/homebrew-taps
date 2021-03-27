a39b08a33d0bd66273af370f47f596ff2c54319c
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-03-27 10:27:44 -0300

    chore: bump kontrolio to 0.32.0

--- kontrolio.rb
+++ kontrolio.rb
@@ -1,8 +1,8 @@
 class Kontrolio < Formula
   desc "Time tracking through your CLI."
   homepage "https://kontrolio.com"
-  url "https://github.com/ktrlio/kontrolio-cli/releases/download/v0.0.31/kontrolio-cli_0.0.31_darwin_amd64.tar.gz"
-  sha256 "5c134adafa7e8fac82c26bc7840707946fabaea90eb6bffc0ef18b56c14579d3"
+  url "https://github.com/ktrlio/kontrolio-cli/releases/download/v0.0.32/kontrolio-cli_0.0.32_darwin_amd64.tar.gz"
+  sha256 "6aa49bb166fe901aa77bfb18df01eefe8ee5424c6af364a5e8202b8f27abd34b"
   license "MIT"
 
   def install
